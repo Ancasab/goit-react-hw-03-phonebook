4cff2a9c42e240f4e4d940f004ed7aeb8bf26d47
Anca_Sab <asabadeanu@gmail.com>
2024-10-15 13:29:31 +0300

    task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,81 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
-
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
 import { Container, Title, Heading2 } from './App.styled';
 
-export const  App = () => { 
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+  }
 
-  const addContact = (newContact) => {
-    setContacts([...contacts, newContact]);
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = (newContact) => {
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
   };
 
-  const deleteContact = (id) => {
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
-    setContacts(updatedContacts);
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
   };
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  setFilter = (filter) => {
+    this.setState({ filter });
+  };
 
-  return (
-    <Container>
-      <Title>Phonebook &#9743; </Title>
-      <ContactForm addContact={addContact} contacts={contacts} />
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
-      <Heading2>Contacts</Heading2>
-      <Filter filter={filter} setFilter={setFilter} />
-      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
-    </Container>
-  );
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <Container>
+        <Title>Phonebook &#9743;</Title>
+        <ContactForm addContact={this.addContact} contacts={this.state.contacts} />
+
+        <Heading2>Contacts</Heading2>
+        <Filter filter={this.state.filter} setFilter={this.setFilter} />
+        <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
+      </Container>
+    );
+  }
 }
 
 export default App;
 
 
+// Fata de hw-02-phonebook am transformat componenta dintr-o componentă funcțională într-o componentă bazată pe clase, deoarece componentDidMount și componentDidUpdate sunt disponibile doar în componentele de clasă.
 
-// Acest cod definește componenta principală App dintr-o aplicație React care gestionează o agendă telefonică. Aplicația permite utilizatorilor să adauge contacte, să filtreze contactele existente și să le șteargă. Iată o scurtă descriere a principalelor părți ale codului:
+//   componentDidMount: Această metodă este folosită pentru a încărca contactele salvate în localStorage după ce componenta a fost montată (similar cu modul în care useEffect ar funcționa cu o dependință goală []).
 
-// Explicația funcționalităților:
-// State-ul local:
+// componentDidUpdate: Această metodă este apelată de fiecare dată când componenta se actualizează. Verificăm dacă contactele din state s-au modificat comparativ cu starea anterioară (prevState.contacts). Dacă s-au modificat, actualizăm localStorage cu noile contacte.
 
-// contacts: O listă de contacte stocată în state, cu fiecare contact având un id, un name și un number. Această listă este inițializată cu câteva contacte de exemplu.
-// filter: Un string care este folosit pentru a căuta contacte pe baza numelui.
-// Funcții principale:
-
-// addContact: Adaugă un nou contact la lista de contacte existentă.
-// deleteContact: Șterge un contact specific din lista de contacte pe baza id-ului.
-// filteredContacts: Aplică filtrul pe lista de contacte și returnează doar contactele care conțin string-ul de căutare în nume (indiferent de majuscule).
-// Componente importate:
-
-// ContactForm: Un formular pentru adăugarea unui nou contact. Aceasta va apela funcția addContact.
-// ContactList: Afișează lista contactelor filtrate și permite ștergerea unui contact.
-// Filter: Oferă funcționalitatea de căutare pentru filtrarea contactelor.
-// Structură vizuală:
-
-// Container, Title, și Heading2: Elemente stilizate care structurează aspectul aplicației.
